0c23570a860e7809c71aeb487d0c44156d54f0ef
chumarjamil <imumarj@gmail.com>
2022-12-05 18:52:40 +0700

    delete function

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
@@ -8,9 +8,19 @@ export class ItemsController {
         return 'This action returns all items';
     }
 
+    @Get(':id')
+    findOne(@Param('id') id): string {
+        return `Item ${id}`
+    }
+
     @Post()
     create(@Body() createItemDto: CreateItemDto): string {
         return `Name is: ${createItemDto.name}`;
+    }
+
+    @Delete('id')
+    delete(@Param('id') id): string {
+        return `Delete ${id}`
     }
 
 }
